17dc314484ab257ce6d0f12e24941eb392c40088
Andrew Murray <andrewmurray.bellevueuniversity@gmail.com>
2020-07-20 15:06:23 -0500

    03.01

--- src/HelloWorld.groovy
+++ src/HelloWorld.groovy
@@ -5,14 +5,9 @@ class HelloWorld {
         johnDoe.setLastName("Doe")
         johnDoe.setAge(40)
 
-        // Catch unchecked exception
-        try {
-            // Convert a String into a Long data type
-            johnDoe.getFirstName().toLong()
-        } catch (NumberFormatException e) {
-            // Handle unchecked exception by printing a message
-            assert e instanceof NumberFormatException
-            println "Cannot convert a String into a Long"
-        }
+        assert johnDoe.toString() == "Person(John, Doe, 40)"
+        assert johnDoe.equals(johnDoe)
+        assert !johnDoe.equals(new Person(firstName: "Mary", lastName: "Hill", age: 30))
+        assert new Person("Mary", "Hill", 30).toString() == "Person(Mary, Hill, 30)"
     }
 }
