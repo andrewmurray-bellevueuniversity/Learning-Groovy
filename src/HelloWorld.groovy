1ef84fdcebb6a38ceb01c904442658c86d7bcaad
Andrew Murray <andrewmurray.bellevueuniversity@gmail.com>
2020-07-20 14:52:31 -0500

    02.04

--- src/HelloWorld.groovy
+++ src/HelloWorld.groovy
@@ -1,13 +1,25 @@
 class HelloWorld {
-    static void main(String[] args) {
-        // Create new Person class and instantiate it
-        Person person = new Person()
-        person.setFirstName("John")
-        person.setLastName("Doe")
-        person.setAge(40)
+	static void main(String[] args) {
+		Person johnDoe = new Person()
+		johnDoe.setFirstName("John")
+		johnDoe.setLastName("Doe")
+		johnDoe.setAge(40)
+		println johnDoe.getFullName()
+		println johnDoe.getAge()
 
-        // Print the full name and age of Person instance
-        println person.getFullName()
-        println person.getAge()
-    }
+		// Identify if Person is middle-aged using a conditional
+		if (johnDoe.getAge() >= 45 && johnDoe.getAge() <= 65) {
+			println johnDoe.getFullName() + " is a middle-aged person"
+		} else {
+			println johnDoe.getFullName() + " is " + johnDoe.getAge() + " years old"
+		}
+
+		// Define a list of Persons
+		def persons = [johnDoe, new Person(firstName: "Mary", lastName: "Hill")]
+
+		 // Iterate over Person instances in list
+		for (Person p : persons) {
+			println "Person: " + p.getFullName()
+		}
+	}
 }
